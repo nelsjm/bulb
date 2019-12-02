ff3d825721443ab00c087c350954d1b146d59084
Jeff Nelson <nelsjm@gmail.com>
2019-12-02 14:24:40 -0700

    Added static settings file in home dir

--- internal/settings/settings.go
+++ internal/settings/settings.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/user"
 )
 
 func GetURL(name string) (string, error) {
@@ -27,10 +28,16 @@ func AddURL(name string, url string) error {
 }
 
 func GetSettings() (map[string]string, error) {
+	settingFile, err := getSettingFile()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	settings := make(map[string]string)
-	f, err := os.Open("settings.json")
+	f, err := os.Open(settingFile)
 	if os.IsNotExist(err) {
-		cf, err := os.Create("settings.json")
+		cf, err := os.Create(settingFile)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -43,6 +50,7 @@ func GetSettings() (map[string]string, error) {
 	}
 
 	if err != nil {
+		fmt.Println("other error")
 		return settings, err
 	}
 
@@ -57,16 +65,33 @@ func GetSettings() (map[string]string, error) {
 }
 
 func setSettings(settings map[string]string) error {
+	settingFile, err := getSettingFile()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	bits, err := json.Marshal(settings)
 	if err != nil {
 		return err
 	}
 
-	f, err := os.Create("settings.json")
+	f, err := os.Create(settingFile)
 	if err != nil {
 		return err
 	}
 
 	_, err = f.Write(bits)
 	return err
+}
+
+func getSettingFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+
+	dir := usr.HomeDir
+
+	return fmt.Sprintf("%s/.bulbs", dir), nil
 }
